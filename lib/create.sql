efca9410b6d0776305c0459b58f13b601e98c61c
klbennett <https://github.com/settings/applications>
2018-09-03 17:21:26 +0100

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE owners
+(id INTEGER PRIMARY KEY,
+title TEXT,
+category INTEGER,
+start TEXT
+end TEXT);
+
+CREATE TABLE user
+(id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+ );
+
+ CREATE TABLE pledge
+ (user_id INTEGER PRIMARY KEY,
+ cat_id  TEXT,
+ age INTEGER,
+  );
